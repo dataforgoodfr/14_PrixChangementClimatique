ecd8c54eb3168cce741fa3d7e0436b8c6aeff1f2
Baptiste Darnala <darnala.b@gmail.com>
2026-02-10 19:37:28 +0100

    Ajout du téléchargement des budget des communes et upload sur le S3 avec fonctions associées et ajout au dbt avec formatage colonne en snake_case

---
+++ data/utils/s3_connector.py
@@ -0,0 +1,56 @@
+import boto3
+from pathlib import Path
+
+import os
+
+from dotenv import load_dotenv
+
+def connect_to_s3(
+    endpoint_url: str,
+    access_key_id: str,
+    secret_access_key: str,
+    region_name: str
+):
+    """ Créer un client de connexion avec un S3
+    """
+    try:
+        s3_client = boto3.client(
+            's3',
+            endpoint_url=endpoint_url,
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+            region_name=region_name
+        )
+        return s3_client
+    except Exception as e:
+        print(f"Erreur de connexion : {e}")
+        return None
+
+def send_file_to_s3(
+    s3_client: boto3.client,
+    bucket: str,
+    filepath: str,
+    s3_filepath: str,
+):
+    """Dépose un fichier sur un bucket S3
+    """
+    with open(filepath, 'rb') as f:  # ✅ Ouvrir en mode binaire
+        s3_client.put_object(
+            Bucket=bucket,
+            Body=f,
+            Key=s3_filepath,
+            ACL='public-read' 
+        )
+    
+if __name__ == "__main__":
+
+    current_dir = Path.cwd()
+    load_dotenv(current_dir / ".env")
+
+    s3_client = connect_to_s3(
+        endpoint_url=os.getenv('S3_ENDPOINT_URL'),
+        access_key_id=os.getenv('S3_ACCESS_KEY'),
+        secret_access_key=os.getenv('S3_SECRET_ACCESS_KEY'),
+        region_name=os.getenv('S3_REGION')
+    )
+    print(s3_client.list_objects_v2(Bucket="qppcc-upload"))
