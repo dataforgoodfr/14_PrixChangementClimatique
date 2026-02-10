9a7a705488766dee8029416a13a0743a9b2b8cbb
Thierry MARAJO <thierry_ma@hotmail.fr>
2026-02-10 10:35:35 +0100

    modification de bronze rei

--- data/dbt_pipeline/models/bronze/bronze_rei.sql
+++ data/dbt_pipeline/models/bronze/bronze_rei.sql
@@ -1,1 +1,9 @@
-SELECT * FROM 'pipeline_inputs/impots_REI_2022.csv'
+{{ config(materialized='table') }}
+
+SELECT *
+FROM read_csv_auto(
+  'pipeline_inputs/impots_REI_2022.csv',
+  header=True,
+  delim=';',
+  encoding='ISO-8859-1'
+)
