6c67672897c88005f9ef74eac1fcca0434d22e32
BiaPri <36563911+BiaPri@users.noreply.github.com>
2026-02-10 12:50:05 +0100

    Update ccr_main_page.sql
    Modifying sql structure to respect template

--- data/dbt_pipeline/models/bronze/ccr_main_page.sql
+++ data/dbt_pipeline/models/bronze/ccr_main_page.sql
@@ -1,1 +1,9 @@
-SELECT * FROM 'pipeline_inputs/ccr_main_page.csv'
+-- ccr_main_page.sql
+
+SELECT 
+  nomPeril,
+  dateArrete,
+  dateParutionJO,
+  codeNOR,
+  codeArrete
+FROM 'pipeline_inputs/ccr_main_page.csv'
