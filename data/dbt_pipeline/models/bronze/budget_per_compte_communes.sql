9adc9f88f1aab2e504f0f29e6ea1513ae8f6cdcf
chloebarre <chloe@sandbag.be>
2026-02-18 00:03:34 +0100

    changement de tables, tables primes assurances et table budget groupee et sommee par code insee et type de compte

--- data/dbt_pipeline/models/bronze/budget_per_compte_communes.sql
+++ data/dbt_pipeline/models/bronze/budget_per_compte_communes.sql
@@ -10,8 +10,9 @@ SELECT
     NDEPT AS code_departement,
     INSEE AS code_insee,
     CREGI AS code_region,
-    COMPTE AS compte,
+    type_compte AS type_compte,
     SD AS solde_debiteur,
     SC AS solde_crediteur,
+    (SD - SC) AS solde,
     (CAST(NDEPT AS TEXT) || CAST(INSEE AS TEXT)) AS code_geo
-FROM 'pipeline_inputs/primes_assurances_communes.csv'
+FROM 'pipeline_inputs/budget_per_compte_communes.csv'
--- data/dbt_pipeline/models/bronze/dettes_communes.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM 'pipeline_inputs/dettes_communes.csv'
