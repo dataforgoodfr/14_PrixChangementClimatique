611e7c6d0e24436e037d641bb2fc3c06120b3a99
BiaPri <principe.biagio@gmail.com>
2026-02-18 09:23:16 +0100

    Requested updates
    Adding code_geo and snake_case

--- data/dbt_pipeline/models/bronze/ccr_main_page.sql
+++ data/dbt_pipeline/models/bronze/ccr_main_page.sql
@@ -1,9 +1,10 @@
 -- ccr_main_page.sql
+-- Use alias to respect naming convention : snake_case
 
 SELECT 
-  nomPeril,
-  dateArrete,
-  dateParutionJO,
-  codeNOR,
-  codeArrete
+  nomPeril AS nom_peril,
+  dateArrete AS date_arrete,
+  dateParutionJO AS date_parution_jo,
+  codeNOR AS code_nor,
+  codeArrete AS code_arrete
 FROM 'pipeline_inputs/ccr_main_page.csv'
