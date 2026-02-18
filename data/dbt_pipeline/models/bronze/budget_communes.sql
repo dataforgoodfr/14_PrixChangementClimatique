2c5f161e95440455c1cfed6e87ffa22ad590c8d4
20k-P <11522296+20k-P@users.noreply.github.com>
2026-02-18 13:00:50 +0100

    fix: code_insee_commune to code_geo for budget_communes

--- data/dbt_pipeline/models/bronze/budget_communes.sql
+++ data/dbt_pipeline/models/bronze/budget_communes.sql
@@ -6,7 +6,7 @@
 
 SELECT 
     "Exercice" as annee,
-    "Code Insee 2024 Commune" as code_insee_commune,
+    "Code Insee 2024 Commune" as code_geo,
     "Siret Budget" as siret_budget,
     "Libellé Budget" as libelle_budget,
     "Type de budget" as type_budget,
