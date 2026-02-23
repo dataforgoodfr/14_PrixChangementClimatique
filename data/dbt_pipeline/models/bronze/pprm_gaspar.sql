1900703a0f6dff0feca08a4992cf50e9d9d096b9
Marie Groshens <165828560+littlecoffe8@users.noreply.github.com>
2026-02-23 14:05:03 +0100

    harmonize snake_case and code_geo in models

--- data/dbt_pipeline/models/bronze/pprm_gaspar.sql
+++ data/dbt_pipeline/models/bronze/pprm_gaspar.sql
@@ -1,2 +1,38 @@
 
-SELECT * FROM 'pipeline_inputs/pprm_gaspar.csv'
+SELECT
+
+    "CODE MODELE" as code_modele,
+    "LIBELLE MODELE" as libelle_modele,
+    "CODE PROECEDURE" as code_procedure,
+    "LIBELLE PROCEDURE" as libelle_procedure,
+    "LIBELLE ORGANISME" as libelle_organisme,
+    "BASSIN RISQUE" as bassin_risque,
+    "BASSINS HYDROGRAPHIQUES" as bassins_hydrographiques,
+    "COURS EAU" as cours_eau,
+    "CODE INSEE COMMUNE" as code_geo, -- code commune - appelé code_geo dans les données bronze du projet
+    "CODE RISQUE 1" as code_risque_1,
+    "LIBELLE RISQUE 1" as libelle_risque_1,
+    "CODE RISQUE 2" as code_risque_2,
+    "LIBELLE RISQUE 2" as libelle_risque_2,
+    "CODE RISQUE 2_1" as code_risque_2_1,
+    "LIBELLE RISQUE 3" as libelle_risque_3,
+    "PROCEDURE REVISANTE" as procedure_revisante,
+    "CODES REVISES" as codes_revises,
+    "PROCEDURE REVISEE" as procedure_revisee,
+    "CODES REVISANTS" as codes_revisants,
+    "PRESCRIPTION" as prescription,
+    "ENQUETE_PUBL_DEBUT" as enquete_publ_debut,
+    "ENQUETE_PUBL_FIN" as enquete_publ_fin,
+    "ANNEX_PLU" as annex_plu,
+    "DEPRESCRIPTION" as deprescription,
+    "APPROBATION" as approbation,
+    "ANNULATION" as annulation,
+    "ABROGATION" as abrogation,
+    "LIBELLE ETAT" as libelle_etat,
+    "DATE ETAT" as date_etat,
+    "LIBELLE SOUS-ETAT" as libelle_sous_etat,
+    "DATE SOUS-ETAT" as date_sous_etat,
+    "DATE DERNIERE MISE A JOUR" as date_derniere_mise_a_jour
+
+
+ FROM 'pipeline_inputs/pprm_gaspar.csv'
--- data/dbt_pipeline/models/bronze/risq_gaspar.sql
+++ data/dbt_pipeline/models/bronze/risq_gaspar.sql
@@ -1,2 +1,9 @@
-
-SELECT * FROM read_csv_auto('pipeline_inputs/risq_gaspar.csv', columns={'num_risque': 'VARCHAR'})
+SELECT
+    cod_commune as code_geo,
+    CAST(num_risque AS VARCHAR) as num_risque,
+    lib_commune,
+    lib_risque
+FROM read_csv_auto(
+    'pipeline_inputs/risq_gaspar.csv',
+    types={'num_risque': 'VARCHAR'}
+)
