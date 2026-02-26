68b780653d0aa5c14b3efe9d4a6cb3f83d2b031c
jluongg <jimmy.luong.pro@gmail.com>
2026-02-26 21:27:47 +0100

    feat(data): ingest and materialize communes

--- data/dbt_pipeline/models/bronze/communes.sql
+++ data/dbt_pipeline/models/bronze/communes.sql
@@ -1,29 +1,29 @@
 -- communes.sql
 -- Table des communes françaises avec géométries spatiales (polygones) et données complémentaires
--- Source: HuWise / Georef France Commune
+-- Source: OpenDataSoft / Georef France Commune (processed via ingest_communes.py)
 
 SELECT
-    com_code[1] AS code_commune,
-    com_name[1] AS nom_commune,
-    com_current_code[1] AS code_commune_actuel,
-    com_name_upper AS nom_commune_majuscule,
-    com_name_lower AS nom_commune_minuscule,
-    com_area_code AS code_zone_superficie,
-    com_type AS type_commune,
-    com_siren_code AS code_siren,
-    com_is_mountain_area AS zone_montagne,
-    dep_code[1] AS code_departement,
-    dep_name[1] AS nom_departement,
-    reg_code[1] AS code_region,
-    reg_name[1] AS nom_region,
-    arrdep_code[1] AS code_arrondissement_departemental,
-    arrdep_name[1] AS nom_arrondissement_departemental,
-    epci_code[1] AS code_epci,
-    epci_name[1] AS nom_epci,
-    ze2020_code[1] AS code_zone_emploi_2020,
-    ze2020_name[1] AS nom_zone_emploi_2020,
-    bv2022_code[1] AS code_bassin_vie_2022,
-    bv2022_name[1] AS nom_bassin_vie_2022,
+    code_commune,
+    nom_commune,
+    code_commune_actuel,
+    nom_commune_majuscule,
+    nom_commune_minuscule,
+    code_zone_superficie,
+    type_commune,
+    code_siren,
+    zone_montagne,
+    code_departement,
+    nom_departement,
+    code_region,
+    nom_region,
+    code_arrondissement_departemental,
+    nom_arrondissement_departemental,
+    code_epci,
+    nom_epci,
+    code_zone_emploi_2020,
+    nom_zone_emploi_2020,
+    code_bassin_vie_2022,
+    nom_bassin_vie_2022,
     geo_point_2d,
-    geom AS geometry
-FROM ST_READ('https://hub.huwise.com/api/explore/v2.1/catalog/datasets/georef-france-commune/exports/geojson')
+    geometry
+FROM 'pipeline_inputs/communes.csv'
