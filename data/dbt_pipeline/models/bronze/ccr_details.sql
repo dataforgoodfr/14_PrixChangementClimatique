812e050960d61e9d65b7403f7ca5c270ab2baae1
chloebarre <chlo.barre@gmail.com>
2026-03-06 16:44:58 +0100

    correct

--- data/dbt_pipeline/models/bronze/ccr_details.sql
+++ data/dbt_pipeline/models/bronze/ccr_details.sql
@@ -4,7 +4,7 @@
 SELECT
     "codeInsee" AS code_geo,
     "nomCommune" AS nom_commune,
-    "dateDebutEvenemen" AS date_debut_evenement,
+    "dateDebutEvenement" AS date_debut_evenement,
     "dateFinEvenement" AS date_fin_evenement,
     "dateArrete" AS date_arrete,
     "dateParutionJO" AS date_parution_jo,
