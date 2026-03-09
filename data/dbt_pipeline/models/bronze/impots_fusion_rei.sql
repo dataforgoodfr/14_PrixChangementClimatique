a5601ec7265627e4e4d74240753ce341d049a31a
Thierry MARAJO <thierry_ma@hotmail.fr>
2026-03-09 18:57:41 +0100

    update impots fusion

--- data/dbt_pipeline/models/bronze/impots_fusion_rei.sql
+++ data/dbt_pipeline/models/bronze/impots_fusion_rei.sql
@@ -1,5 +1,2 @@
-SELECT 
-    {% for col in adapter.get_columns_in_relation(source('pipeline_inputs', 'impots_fusion_rei')) %}
-    CAST({{ col.column }} AS VARCHAR) AS {{ clean_column_name(col.column) }}{{ "," if not loop.last }}
-    {% endfor %}
-FROM {{ source('pipeline_inputs', 'impots_fusion_rei') }}
+-- models/bronze/test_simple.sql
+SELECT * FROM {{ source('pipeline_inputs', 'impots_fusion_rei') }}
