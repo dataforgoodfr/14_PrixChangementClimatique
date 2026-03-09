5c1d261612b2fd097e62d3f78fa2f4d017e8ee6b
Thierry MARAJO <thierry_ma@hotmail.fr>
2026-03-09 17:10:49 +0100

    Update impots_fusion_rei
    Ajout d'un appel d'une macro pour convertir en snake

--- data/dbt_pipeline/models/bronze/impots_fusion_rei.sql
+++ data/dbt_pipeline/models/bronze/impots_fusion_rei.sql
@@ -1,3 +1,5 @@
-{{ config(materialized='table') }}
-
-SELECT * FROM 'pipeline_inputs/impots_fusion_rei.csv'
+SELECT 
+    {% for col in adapter.get_columns_in_relation(source('pipeline_inputs', 'impots_fusion_rei')) %}
+    CAST({{ col.column }} AS VARCHAR) AS {{ clean_column_name(col.column) }}{{ "," if not loop.last }}
+    {% endfor %}
+FROM {{ source('pipeline_inputs', 'impots_fusion_rei') }}
