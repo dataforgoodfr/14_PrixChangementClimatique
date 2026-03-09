c4b3217a8972297de3e15fcf8229d8154d7865af
Thierry MARAJO <thierry_ma@hotmail.fr>
2026-03-09 17:37:57 +0100

    Update clean column

--- data/dbt_pipeline/macros/clean_column_name.sql
+++ data/dbt_pipeline/macros/clean_column_name.sql
@@ -1,19 +1,18 @@
 {% macro clean_column_name(column_name) %}
-    {% set cleaned = column_name 
-        | lower 
-        | replace(' ', '_') 
-        | replace('-', '_') 
-        | replace('/', '_') 
-        | replace('.', '_') 
-        | replace(':', '_') 
-        | replace("'", '') 
-        | replace('(', '') 
-        | replace(')', '') 
-        | replace('é', 'e') 
-        | replace('è', 'e') 
-        | replace('ê', 'e') 
-        | replace('à', 'a') 
-    %}
-    {# Appliquer le regex sur la version nettoyée #}
-    {{ cleaned | regex_replace(r'__+', '_') | trim('_') }}
+    {{ column_name
+        | lower
+        | replace(' ', '_')
+        | replace('-', '_')
+        | replace('/', '_')
+        | replace('.', '_')
+        | replace(':', '_')
+        | replace("'", '')
+        | replace('(', '')
+        | replace(')', '')
+        | replace('é', 'e')
+        | replace('è', 'e')
+        | replace('ê', 'e')
+        | replace('à', 'a')
+        | trim('_')
+    }}
 {% endmacro %}
