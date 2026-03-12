40ef374a56d61dafb73d74e9a4af2a086b1bcfc5
Thierry MARAJO <thierry_ma@hotmail.fr>
2026-03-12 15:48:34 +0100

    Update impots fusion rei

--- data/dbt_pipeline/models/bronze/impots_fusion_rei.sql
+++ data/dbt_pipeline/models/bronze/impots_fusion_rei.sql
@@ -1,11 +1,12 @@
+{#- Configuration pour le linter sqlfluff qui ne voit pas les colonnes en dehors de dbt -#}
 {% set cols = adapter.get_columns_in_relation(source('pipeline_inputs', 'impots_fusion_rei')) %}
 
 SELECT
-    {% if cols | length > 0 %}
+    {% if cols %}
         {% for col in cols -%}
             "{{ col.name }}" AS {{ clean_column_name(col.name) }}{% if not loop.last %},{% endif %}
         {% endfor %}
-    {% else %}
+    {%- else -%}
         *
-    {% endif %}
+    {%- endif %}
 FROM {{ source('pipeline_inputs', 'impots_fusion_rei') }}
