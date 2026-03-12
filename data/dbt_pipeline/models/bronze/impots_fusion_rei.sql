d19a69b6b76c9b0d265145201e75cff7226f68b8
Thierry MARAJO <thierry_ma@hotmail.fr>
2026-03-12 19:45:59 +0100

    Update impots fusion rei

--- data/dbt_pipeline/models/bronze/impots_fusion_rei.sql
+++ data/dbt_pipeline/models/bronze/impots_fusion_rei.sql
@@ -1,7 +1,17 @@
 {{ config(materialized='view') }}
 
-SELECT *
-FROM read_csv_auto('pipeline_inputs/impots_fusion_rei.csv', 
- all_varchar=true
- )
- 
+WITH raw_data AS (
+    SELECT *
+    FROM read_csv_auto(
+        'pipeline_inputs/impots_fusion_rei.csv',
+        all_varchar=true
+    )
+)
+
+SELECT 
+    {%- for column_name in adapter.get_columns_in_relation(ref('impots_fusion_rei')) %}
+        -- On applique ta macro sur chaque nom de colonne
+        {{ column_name.name }} AS {{ clean_column_name(column_name.name) }}
+        {%- if not loop.last %},{% endif %}
+    {%- endfor %}
+FROM raw_data
