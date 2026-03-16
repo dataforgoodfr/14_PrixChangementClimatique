3a46539b59ff4fdb2ce6d0b83d6e5b6003db8be5
Thierry MARAJO <thierry_ma@hotmail.fr>
2026-03-16 20:34:03 +0100

    Update impots fusion reu

--- data/dbt_pipeline/models/bronze/impots_fusion_rei.sql
+++ data/dbt_pipeline/models/bronze/impots_fusion_rei.sql
@@ -1,6 +1,6 @@
 SELECT
-    IDCOM AS code_geo,
-    * EXCLUDE (IDCOM)
+    * EXCLUDE (idcom),
+    idcom AS code_geo
 FROM read_csv_auto(
     'pipeline_inputs/impots_fusion_rei.csv',
     all_varchar = true
