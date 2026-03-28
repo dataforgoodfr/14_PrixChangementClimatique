3c5030fc3c2f2f8c4ea1a74a6d21d476501d65dc
chloebarre <chlo.barre@gmail.com>
2026-03-28 16:30:56 +0100

    CTE et ref

--- data/dbt_pipeline/models/silver/scenario_2050.sql
+++ data/dbt_pipeline/models/silver/scenario_2050.sql
@@ -1,11 +1,5 @@
-SELECT
-    c.code_geo,
-    s.nortx_35_d_yr,
-    s.norrrq_99_ref_d_yr,
-    s.norswi_04_yr,
-    s.nortr_yr,
-    s.norrr_yr
-FROM (
+WITH
+geo_com AS (
     SELECT
         *,
         (geo_point_2_d::json ->> 'lon')::double AS lon,
@@ -14,9 +8,20 @@ FROM (
             (geo_point_2_d::json ->> 'lat')::double,
             (geo_point_2_d::json ->> 'lon')::double
         ] AS geom_array
-    FROM opendatasoft_communes
-) AS c
-LEFT JOIN LATERAL (
+    FROM {{ ref('opendatasoft_communes') }}
+)
+
+SELECT
+    c.code_geo,
+    s.nortx_35_d_yr,
+    s.norrrq_99_ref_d_yr,
+    s.norswi_04_yr,
+    s.nortr_yr,
+    s.norrr_yr
+FROM 
+    geo_com  AS c
+LEFT JOIN LATERAL 
+     (
     SELECT
         s.longitude,
         s.latitude,
@@ -25,10 +30,11 @@ LEFT JOIN LATERAL (
         s.norswi_04_yr,
         s.nortr_yr,
         s.norrr_yr
-    FROM drias AS s
+    FROM {{ ref('drias') }} AS s
     WHERE
         s.longitude BETWEEN (c.lon - 1) AND (c.lon + 1)
         AND s.latitude BETWEEN (c.lat - 1) AND (c.lat + 1)
-    ORDER BY c.geom_array <-> ARRAY[s.latitude, s.longitude]  -- noqa
+ORDER BY c.geom_array <-> ARRAY[s.latitude, s.longitude]  -- noqa
     LIMIT 1
-) AS s ON TRUE
+) AS s
+ ON TRUE
