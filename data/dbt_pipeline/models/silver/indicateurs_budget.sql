f75ae1770a82e4bdf76d8ebd42b50ac0efe8c130
20k-P <11522296+20k-P@users.noreply.github.com>
2026-03-28 17:37:52 +0100

    feat: remove prime, rename code_geo using COALESCE

--- data/dbt_pipeline/models/silver/indicateurs_budget.sql
+++ data/dbt_pipeline/models/silver/indicateurs_budget.sql
@@ -6,9 +6,8 @@ SELECT
     budget.produits - budget.depenses AS solde_annuel,
     budget.dettes / budget.produits AS ratio_dettes_produits,
     budget.dettes / budget.depenses AS ratio_dettes_depenses,
-    budget.primes / budget.depenses AS ratio_primes_depenses,
+    budget.produits / budget.depenses AS ratio_produits_depenses,
     budget.dettes / pop.population AS dettes_per_pop,
-    budget.primes / pop.population AS primes_per_pop,
     budget.depenses / pop.population AS depenses_per_pop,
     budget.produits / pop.population AS produits_per_pop,
     (budget.produits - budget.depenses) / pop.population AS solde_annuel_per_pop
@@ -17,5 +16,5 @@ FROM
 LEFT JOIN
     {{ ref('population_par_com_annee') }} AS pop
     ON
-        budget.code_geo_from_siren = pop.code_geo
+        budget.code_geo = pop.code_geo
         AND budget.annee = pop.annee
