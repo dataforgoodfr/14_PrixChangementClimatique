d7f88d72390ed80dc4e565d2990e75d5a88b32b8
20k-P <11522296+20k-P@users.noreply.github.com>
2026-03-28 14:39:26 +0100

    fix: typo sur primes d assurances

--- data/dbt_pipeline/models/silver/budget_par_com_annee.sql
+++ data/dbt_pipeline/models/silver/budget_par_com_annee.sql
@@ -5,13 +5,13 @@ SELECT
     nom_com,
     annee,
     SUM(CASE WHEN type_compte = 'dettes financieres' THEN solde ELSE 0 END) AS dettes,
-    SUM(CASE WHEN type_compte = 'primes d ASsurances' THEN solde ELSE 0 END) AS primes,
+    SUM(CASE WHEN type_compte = 'primes d assurances' THEN solde ELSE 0 END) AS primes,
     SUM(CASE WHEN type_compte = 'depenses' THEN solde ELSE 0 END) AS depenses,
     SUM(CASE WHEN type_compte = 'produits' THEN solde ELSE 0 END) AS produits
 FROM
     {{ ref('budget_per_compte_communes') }}
 WHERE
-    type_compte IN ('dettes financieres', 'primes d ASsurances', 'depenses', 'produits')
+    type_compte IN ('dettes financieres', 'primes d assurances', 'depenses', 'produits')
 GROUP BY
     code_geo_from_siren,
     nom_com,
