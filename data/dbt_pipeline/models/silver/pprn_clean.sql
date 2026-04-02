296c5c3be258311d1ee93f839d7fb1fa3a22a9eb
BiaPri <principe.biagio@gmail.com>
2026-04-02 16:06:25 +0200

    Creating pprn_clean table

---
+++ data/dbt_pipeline/models/silver/pprn_clean.sql
@@ -0,0 +1,27 @@
+-- pprn_clean.sql
+
+-- Filtrer PPRN actifs
+WITH active_pprn AS (
+    SELECT *
+    FROM {{ ref('pprn_gaspar') }}
+    WHERE libelle_etat = 'Opposable'
+),
+
+-- Approbation plus recentes
+last_update AS (
+    SELECT *,
+        ROW_NUMBER() OVER(
+            PARTITION BY code_modele, code_geo
+            ORDER BY CAST(approbation AS TIMESTAMP) DESC
+        ) AS rn
+    FROM active_pprn
+)
+
+SELECT 
+    code_geo,
+    REPLACE(code_modele, 'PPRN-', '') AS pprn,
+    libelle_risque_2 as pprn_libelle,
+    libelle_risque_3 as pprn_desc,
+    approbation as date_approbation,
+FROM last_update
+WHERE rn = 1;
