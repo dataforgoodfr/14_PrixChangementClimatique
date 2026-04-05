566e8db965d7c0d43ab59421fbc508e2c3ccd8d7
20k-P <11522296+20k-P@users.noreply.github.com>
2026-04-05 11:17:07 +0200

    feat: update CI to exclude test from check-model-has-description

--- data/dbt_pipeline/tests/test_batiments_risk_RGA.sql
+++ data/dbt_pipeline/tests/test_batiments_risk_RGA.sql
@@ -1,11 +1,12 @@
 SELECT
-    code_geo, rga_score
-FROM 
-     {{ ref('batiments_risk_RGA') }}
+    code_geo,
+    rga_score
+FROM
+    {{ ref('batiments_risk_RGA') }}
 WHERE
-    (code_geo = 1229 AND ROUND(rga_score,3) != 3.36)
-    AND (code_geo = 95675 AND ROUND(rga_score,4) != 0.8286)
-    AND (code_geo = 95379 AND ROUND(rga_score,2) != 4.19)
-    AND (code_geo = 1134 AND ROUND(rga_score,2) != 3.39)
-    AND (code_geo = 1157 AND ROUND(rga_score,2) != 0.58)
-    AND (code_geo = 1159 AND ROUND(rga_score,2) != 2.50)
+    (code_geo = 1229 AND ROUND(rga_score, 3) != 3.36)
+    AND (code_geo = 95675 AND ROUND(rga_score, 4) != 0.8286)
+    AND (code_geo = 95379 AND ROUND(rga_score, 2) != 4.19)
+    AND (code_geo = 1134 AND ROUND(rga_score, 2) != 3.39)
+    AND (code_geo = 1157 AND ROUND(rga_score, 2) != 0.58)
+    AND (code_geo = 1159 AND ROUND(rga_score, 2) != 2.50)
--- data/dbt_pipeline/tests/test_batiments_risk_TRI.sql
+++ data/dbt_pipeline/tests/test_batiments_risk_TRI.sql
@@ -1,11 +1,12 @@
 SELECT
-    code_geo, tri_score
-FROM 
-     {{ ref('batiments_risk_TRI') }}
+    code_geo,
+    tri_score
+FROM
+    {{ ref('batiments_risk_TRI') }}
 WHERE
-    (code_geo = 1030 AND ROUND(tri_score,3) != 1.347)
-    AND (code_geo = 1043 AND ROUND(tri_score,4) != 0.0038)
-    AND (code_geo = 1123 AND ROUND(tri_score,2) != 0.84)
-    AND (code_geo = 1134 AND ROUND(tri_score,2) != 0.38)
-    AND (code_geo = 1157 AND ROUND(tri_score,2) != 0.14)
-    AND (code_geo = 1159 AND ROUND(tri_score,2) != 0.87)
+    (code_geo = 1030 AND ROUND(tri_score, 3) != 1.347)
+    AND (code_geo = 1043 AND ROUND(tri_score, 4) != 0.0038)
+    AND (code_geo = 1123 AND ROUND(tri_score, 2) != 0.84)
+    AND (code_geo = 1134 AND ROUND(tri_score, 2) != 0.38)
+    AND (code_geo = 1157 AND ROUND(tri_score, 2) != 0.14)
+    AND (code_geo = 1159 AND ROUND(tri_score, 2) != 0.87)
