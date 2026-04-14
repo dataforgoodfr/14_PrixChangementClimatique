78557611b2492a5cce6fb9c7fe6e23df16aa6016
Maxime Lévêque <maxlev.oclock@gmail.com>
2026-04-14 16:16:39 +0200

    fix linter 4

--- data/dbt_pipeline/models/silver/indicateurs_tri_rga_bats_par_com.sql
+++ data/dbt_pipeline/models/silver/indicateurs_tri_rga_bats_par_com.sql
@@ -1,4 +1,3 @@
-
 -- PREMIERE ETAPE : faire les calculs pour l'indicateur TRI
 WITH table_tri AS (
     SELECT
@@ -9,8 +8,7 @@ WITH table_tri AS (
             + nb_bats_service_risque_faible + 3 * nb_bats_service_risque_moyen + 5 * nb_bats_service_risque_fort
             + nb_bats_indus_risque_faible + 3 * nb_bats_indus_risque_moyen + 5 * nb_bats_indus_risque_fort
             + nb_bats_autres_risque_faible + 3 * nb_bats_autres_risque_moyen + 5 * nb_bats_autres_risque_fort
-        ) / (nb_bats_total * 5) AS indicateur_tri
-        
+        ) / (nb_bats_total * 5) AS indicateur_tri        
     FROM {{ ref('tri_bats_communes') }}
 ),
 
@@ -42,11 +40,9 @@ table_rga AS (
                 + nb_maisons_inconnu_risque_moyen * 5
                 + nb_maisons_inconnu_risque_fort * 10
             )
-        ) / (nb_maisons_total * 10) AS indicateur_rga
-        
+        ) / (nb_maisons_total * 10) AS indicateur_rga        
     FROM {{ ref('rga_bats_communes') }}
 )
-
 
 -- TROISIEME ETAPE : joindre les deux tables pour avoir la table finale
 -- (étant donné qu'il n'y a pas les DROMs pour le rga, la valeur d'indicateur rga sera null pour ces communes)
