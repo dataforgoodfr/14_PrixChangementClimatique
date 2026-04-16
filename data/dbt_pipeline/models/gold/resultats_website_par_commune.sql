641b8e47af22eefa47919557b5d91604c835c55d
Marie Groshens <165828560+littlecoffe8@users.noreply.github.com>
2026-04-16 11:41:19 +0200

    cast geometry as geometry

--- data/dbt_pipeline/models/gold/resultats_website_par_commune.sql
+++ data/dbt_pipeline/models/gold/resultats_website_par_commune.sql
@@ -10,14 +10,15 @@ SELECT
     c.nom_departement AS departement,
     c.nom_region AS region,
     c.geo_point_2_d,
-    c.geometry,
 
     -- KPI randoms récupérés depuis la table indices
+    i.score_economique,
     i.score_georisque,
     i.score_assurance,
     i.indice_vulnerabilite,
     i.indice_vulnerabilite_niveau,
-    i.score_economique
+
+    CAST(c.geometry AS geometry) AS geometry --noqa
 
 FROM {{ ref('opendatasoft_communes') }} AS c
 LEFT JOIN {{ ref('indice_par_commune') }} AS i
