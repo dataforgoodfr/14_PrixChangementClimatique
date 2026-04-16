8cf0674816adfd5b98e798db7135ed74f88af440
Yacine Zarebski <61387470+Wayzi88@users.noreply.github.com>
2026-04-16 21:04:24 +0200

    Add swr and global error (#95)
    * fix rebase conflicts
    
    * fix commune selection from a click on the map

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
