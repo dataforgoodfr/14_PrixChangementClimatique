58924c756c0034b62ade2c89fd7ae82328f919cb
Thierry MARAJO <thierry_ma@hotmail.fr>
2026-04-23 13:06:39 +0200

    Gold kpi impots (#112)
    * Ajout des kpi impots locaux
    
    * Update de la doc yml
    
    Description des variable des kpi impots
    
    * update resultats website par communes yml
    
    * update resultats website par commune yml
    
    * update resultats website par communes
    
    * update résultats website par commune
    
    * Ajout d'une ligne non présente en local
    
    Conflit avant de merger , ligne GROUP BY code_geo pour pprn absente sur le fichier local
    
    * Update resultats website par communes

--- data/dbt_pipeline/models/gold/resultats_website_par_commune.sql
+++ data/dbt_pipeline/models/gold/resultats_website_par_commune.sql
@@ -62,6 +62,24 @@ pprn AS (
         ) AS date_approbation_ino
     FROM {{ ref('pprn_clean') }}
     GROUP BY code_geo
+),
+
+kpi_impots AS (
+    SELECT
+        code_geo,
+        SUM(CASE WHEN annee = 2024 AND agregat = 'Impôts locaux' THEN montant ELSE 0 END) AS impots_locaux,
+        (
+            SUM(CASE WHEN annee = 2024 AND agregat = 'Impôts locaux' THEN montant ELSE 0 END)
+            - SUM(CASE WHEN annee = 2020 AND agregat = 'Impôts locaux' THEN montant ELSE 0 END)
+        ) / NULLIF(
+            SUM(CASE WHEN annee = 2020 AND agregat = 'Impôts locaux' THEN montant ELSE 0 END), 0
+        ) AS impots_locaux_evolution,
+        SUM(CASE WHEN annee = 2024 AND agregat = 'Impôts locaux' THEN montant ELSE 0 END)
+        / NULLIF(
+            SUM(CASE WHEN annee = 2024 AND agregat = 'Recettes de fonctionnement' THEN montant ELSE 0 END), 0
+        ) AS part_impots_locaux
+    FROM {{ ref('donnees_financieres_ofgl') }}
+    GROUP BY code_geo
 )
 
 SELECT
@@ -107,10 +125,14 @@ SELECT
     pr.date_approbation_rga,
     pr.date_approbation_ino,
     pop.population,
+    i_loc.impots_locaux,
+
+    i_loc.impots_locaux_evolution,
+
+    i_loc.part_impots_locaux,
     pr.pprn_rga IS TRUE AS pprn_rga,
 
     pr.pprn_ino IS TRUE AS pprn_ino,
-
     p.prime_assurance_2024 / b.depenses AS part_prime_budget,
     (p.prime_assurance_2024 - p.prime_assurance_2020) / NULLIF(p.prime_assurance_2020, 0) AS evolution_prime_assurance
 
@@ -141,3 +163,6 @@ LEFT JOIN pprn AS pr
 
 LEFT JOIN {{ ref('indice_par_commune') }} AS i
     ON c.code_geo = i.code_geo
+
+LEFT JOIN kpi_impots AS i_loc
+    ON c.code_geo = i_loc.code_geo
