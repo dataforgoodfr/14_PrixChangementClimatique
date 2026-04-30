345a66311e0c543383a6875345120928f952d613
Wayzi88 <yschewps@gmail.com>
2026-04-30 16:54:42 +0200

    clean

--- website/components/map/economical-situation.tsx
+++ website/components/map/economical-situation.tsx
@@ -1,5 +1,4 @@
 import { Commune } from "@/lib/types/communes";
-import { useMemo } from "react";
 import { StatCard } from "../core/stat-card";
 import { SectionTitle } from "./section-title";
 
@@ -8,54 +7,41 @@ export const EconomicalSituation = ({
 }: {
   selectedCommuneData: Commune;
 }) => {
-  const statCards = useMemo(() => {
-    const stat = [];
-    if (selectedCommuneData.ratio_dettes_depenses) {
-      stat.push({
-        title: "TAUX D'ENDETTEMENT",
-        currentValue:
-          Math.round(selectedCommuneData.ratio_dettes_depenses * 10) / 10,
-        unit: "%",
-      });
-    }
-    if (selectedCommuneData.depenses_per_pop) {
-      stat.push({
-        title: "BUDGET PAR HABITANT",
-        currentValue: Math.round(selectedCommuneData.depenses_per_pop),
-        unit: "€",
-      });
-    }
-    if (selectedCommuneData.part_impots_locaux) {
-      stat.push({
-        title: "IMPOTS LOCAUX / BUDGET",
-        currentValue: Math.round(selectedCommuneData.part_impots_locaux * 100) / 100,
-        unit: "%",
-        variation:
-          selectedCommuneData.impots_locaux_evolution &&
-          Math.round(selectedCommuneData.impots_locaux_evolution * 10) / 10,
-        comparisonText: "2020 vs 2024",
-      });
-    }
-    return stat;
-  }, [selectedCommuneData]);
-
   return (
     <div className="p-4 space-y-5">
       <SectionTitle title="Situation économique" />
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-        {statCards.length > 0 ? (
-          statCards.map((statcard) => (
-            <StatCard
-              key={statcard.title}
-              title={statcard.title}
-              currentValue={statcard.currentValue}
-              unit={statcard.unit}
-              variationPercentage={statcard.variation}
-              comparisonText={statcard.comparisonText}
-            />
-          ))
-        ) : (
-          <p>Chargement ...</p>
+        {selectedCommuneData.ratio_dettes_depenses && (
+          <StatCard
+            title="TAUX D'ENDETTEMENT"
+            currentValue={
+              Math.round(selectedCommuneData.ratio_dettes_depenses * 10) / 10
+            }
+            unit="%"
+          />
+        )}
+
+        {selectedCommuneData.depenses_per_pop && (
+          <StatCard
+            title="BUDGET PAR HABITANT"
+            currentValue={Math.round(selectedCommuneData.depenses_per_pop)}
+            unit="€"
+          />
+        )}
+
+        {selectedCommuneData.part_impots_locaux && (
+          <StatCard
+            title="IMPOTS LOCAUX / BUDGET"
+            currentValue={
+              Math.round(selectedCommuneData.part_impots_locaux * 100) / 100
+            }
+            unit="%"
+            variationPercentage={
+              selectedCommuneData.impots_locaux_evolution &&
+              Math.round(selectedCommuneData.impots_locaux_evolution * 10) / 10
+            }
+            comparisonText="2020 vs 2024"
+          />
         )}
       </div>
     </div>
