470dbe7e93b54900a63d5f54b97ded9be18ab881
Marie Groshens <165828560+littlecoffe8@users.noreply.github.com>
2026-05-05 17:25:25 +0200

    compute real stats in resultats_nationaux

--- data/dbt_pipeline/models/gold/resultats_nationaux.sql
+++ data/dbt_pipeline/models/gold/resultats_nationaux.sql
@@ -32,18 +32,32 @@ primes AS (
         END) AS primes_assurances_2020
 
     FROM {{ ref('primes_par_communes') }}
+),
+
+scores AS (
+    SELECT
+        MEAN(score_economique) AS score_economique_moy,
+        MEAN(score_climatique) AS score_climatique_moy,
+        MEAN(score_assurance) AS score_assurance_moy,
+        MEAN(indice_vulnerabilite) AS indice_vulnerabilite_moy,
+        MEAN(indice_vulnerabilite_niveau) AS indice_vulnerabilite_niveau,
+        SUM(CASE WHEN indice_vulnerabilite_niveau >= 4.0 THEN 1 ELSE 0 END)
+        /
+        COUNT(indice_vulnerabilite_niveau)
+            AS part_communes_vulnerables
+    FROM {{ ref('indice_par_commune') }}
 )
 
 -- CODE COLONNES RANDOM
 
 SELECT
 
-    CAST(RANDOM() AS DECIMAL(3, 2)) AS score_economique_moy,
-    CAST(RANDOM() AS DECIMAL(3, 2)) AS score_georisque_moy,
-    CAST(RANDOM() AS DECIMAL(3, 2)) AS score_assurance_moy,
-    CAST(RANDOM() AS DECIMAL(3, 2)) AS indice_vulnerabilite_moy,
-    CAST(RANDOM() AS INT) AS indice_vulnerabilite_niveau,
-    CAST(RANDOM() AS DECIMAL(3, 2)) AS part_communes_vulnerables,
+    CAST(s.score_climatique_moy AS DECIMAL(3, 2)) AS score_climatique_moy,
+    CAST(s.score_assurance_moy AS DECIMAL(3, 2)) AS score_assurance_moy,
+    CAST(s.score_economique_moy AS DECIMAL(3, 2)) AS score_economique_moy,
+    CAST(s.indice_vulnerabilite_moy AS DECIMAL(3, 2)) AS indice_vulnerabilite_moy,
+    CAST(s.indice_vulnerabilite_niveau AS DECIMAL(3, 2)) AS indice_vulnerabilite_niveau,
+    CAST(s.part_communes_vulnerables AS DECIMAL(3, 2)) AS part_communes_vulnerables,
 
     CAST(p.primes_assurances_2024 AS DECIMAL(15, 2)) AS primes_assurances_2024,
     CAST(p.primes_assurances_2020 AS DECIMAL(15, 2)) AS primes_assurances_2020,
@@ -52,3 +66,4 @@ SELECT
 
 FROM primes AS p
 CROSS JOIN impots AS i
+CROSS JOIN scores AS s
