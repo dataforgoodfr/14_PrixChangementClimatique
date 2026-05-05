2cd014e21c578cd65f9a4f9c996f0a0b2cd7182d
Marie Groshens <165828560+littlecoffe8@users.noreply.github.com>
2026-05-05 11:27:13 +0200

    reset

--- data/dbt_pipeline/models/gold/resultats_website_par_commune.sql
+++ data/dbt_pipeline/models/gold/resultats_website_par_commune.sql
@@ -1,4 +1,4 @@
--- test resultats_par_commune.sql
+-- resultats_par_commune.sql
 -- Gold layer: Résultats enrichis par commune avec KPI temporaire
 -- Source: Bronze layer communes +  KPI
 -- Description: Contient les données communes nécessaires pour le site web
