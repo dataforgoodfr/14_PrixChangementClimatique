446c659b16e947b847d0b661042d9f67691e06f8
chloebarre <chlo.barre@gmail.com>
2026-05-05 09:41:18 +0200

    modification de la population Mayotte et Paris

--- data/dbt_pipeline/models/silver/indicateurs_budget.sql
+++ data/dbt_pipeline/models/silver/indicateurs_budget.sql
@@ -1,5 +1,26 @@
 -- indicateurs_budget.sql
 
+WITH pop_adjusted AS (
+
+    SELECT
+        code_geo,
+        annee_recensement,
+        population
+    FROM {{ ref('population_par_com_annee') }}
+    
+    UNION ALL
+
+    SELECT
+        '75056' AS code_geo,
+        annee_recensement,
+        SUM(population) AS population
+    FROM {{ ref('population_par_com_annee') }}
+    WHERE code_geo LIKE '75%'
+    GROUP BY annee_recensement
+
+)
+
+
 SELECT
     budget.*,
     pop.population,
@@ -13,8 +34,13 @@ SELECT
     (budget.produits - budget.depenses) / pop.population AS solde_annuel_per_pop
 FROM
     {{ ref('budget_par_com_annee') }} AS budget
-LEFT JOIN
-    {{ ref('population_par_com_annee') }} AS pop
-    ON
-        budget.code_geo = pop.code_geo
-        AND budget.annee = pop.annee_recensement
+
+LEFT JOIN pop_adjusted AS pop
+    ON budget.code_geo = pop.code_geo
+    AND (
+        -- Cas 976 → toujours année 2017
+        (budget.code_geo LIKE '976%' AND pop.annee_recensement = 2017)
+
+        -- Autres cas 
+        OR (budget.code_geo NOT LIKE '976%' AND budget.annee = pop.annee_recensement)
+    )
