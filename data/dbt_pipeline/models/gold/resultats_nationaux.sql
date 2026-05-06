d83b6b8a36e2f3c9c6fd71d50597f5273f7f9a0e
Laurent34000 <lafrigole@hotmail.fr>
2026-05-06 17:16:43 +0200

    Ajout variable table resultats_nationaux

--- data/dbt_pipeline/models/gold/resultats_nationaux.sql
+++ data/dbt_pipeline/models/gold/resultats_nationaux.sql
@@ -32,12 +32,65 @@ primes AS (
         END) AS primes_assurances_2020
 
     FROM {{ ref('primes_par_communes') }}
+),
+
+-- EVOLUTION PRIME ASSURANCE
+
+evol_prime AS (
+    SELECT
+        (primes_assurances_2024 - primes_assurances_2020)
+        / NULLIF(primes_assurances_2020, 0)
+        * 100 AS evolution_prime_assurance
+
+    FROM primes
+),
+
+-- PART PRIME BUDGET 2024
+
+prime_budget AS (
+    SELECT
+        SUM(CASE
+            WHEN annee = 2024
+                THEN depenses
+            ELSE 0
+        END) AS depenses_2024
+
+    FROM {{ ref('indicateurs_budget') }}
+),
+
+-- DEPENSES PAR POPULATION ANNEE 2023
+
+dep_pop AS (
+    SELECT
+        SUM(CASE
+            WHEN annee = 2023
+                THEN
+                    CASE
+                        WHEN NOT ISFINITE(depenses_per_pop) THEN NULL
+                        ELSE depenses_per_pop
+                    END
+            ELSE 0
+        END) AS depenses_per_pop
+
+    FROM {{ ref('indicateurs_budget') }}
+),
+
+-- RATIO DETTES DEPENSE
+
+ratio_dette_dep AS (
+    SELECT
+        SUM(CASE
+            WHEN annee = 2024
+                THEN dettes
+            ELSE 0
+        END) AS dettes_2024
+
+    FROM {{ ref('indicateurs_budget') }}
 )
 
 -- CODE COLONNES RANDOM
 
 SELECT
-
     CAST(RANDOM() AS DECIMAL(3, 2)) AS score_economique_moy,
     CAST(RANDOM() AS DECIMAL(3, 2)) AS score_georisque_moy,
     CAST(RANDOM() AS DECIMAL(3, 2)) AS score_assurance_moy,
@@ -48,7 +101,24 @@ SELECT
     CAST(p.primes_assurances_2024 AS DECIMAL(15, 2)) AS primes_assurances_2024,
     CAST(p.primes_assurances_2020 AS DECIMAL(15, 2)) AS primes_assurances_2020,
     CAST(i.impots_locaux_2024 AS DECIMAL(15, 2)) AS impots_locaux_2024,
-    CAST(i.impots_locaux_2020 AS DECIMAL(15, 2)) AS impots_locaux_2020
+    CAST(i.impots_locaux_2020 AS DECIMAL(15, 2)) AS impots_locaux_2020,
+    CAST(e.evolution_prime_assurance AS DECIMAL(10, 2)) AS evolution_prime_assurance,
+    CAST(
+        p.primes_assurances_2024
+        / NULLIF(b.depenses_2024, 0) * 100
+        AS DECIMAL(10, 2)
+    ) AS part_prime_budget,
+    CAST(
+        CASE
+            WHEN NOT ISFINITE(d.depenses_per_pop) THEN NULL
+            ELSE d.depenses_per_pop
+        END AS DECIMAL(10, 2)
+    ) AS depenses_per_pop,
+    CAST(r.dettes_2024 / NULLIF(b.depenses_2024, 0) AS DECIMAL(10, 5)) AS ratio_dettes_depenses
 
 FROM primes AS p
 CROSS JOIN impots AS i
+CROSS JOIN evol_prime AS e
+CROSS JOIN prime_budget AS b
+CROSS JOIN dep_pop AS d
+CROSS JOIN ratio_dette_dep AS r
