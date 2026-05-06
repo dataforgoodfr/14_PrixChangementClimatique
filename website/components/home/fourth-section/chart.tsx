a6b07dea0a00af2ac35c02c560b38e5aafdd0a52
Adrien Mulattieri <adrien.mulattieri@icloud.com>
2026-05-06 16:58:49 +0200

    bar chart part des dépenses assurance dans budget communes

--- website/components/home/fourth-section/chart.tsx
+++ website/components/home/fourth-section/chart.tsx
@@ -1,34 +1,25 @@
-"use-client";
-import Image from "next/image";
+"use client";
+import dynamic from "next/dynamic";
 import Caption from "./caption";
 import { Info } from "lucide-react";
+
+const ChartContent = dynamic(() => import("./chart-content"), { ssr: false });
 
 const FourthSection: React.FC = () => {
   return (
     <div className="flex flex-col gap-[16px] w-full p-[16px] border rounded-2xl">
       <div className="flex flex-col gap-[4px]">
         <p className="text-[14px]  md:text-[20px] md:text-[22px] font-[600] text-[#1E1B39]">
-          Répartition de la part des dépenses d'assurance dans le budget des
-          communes en 2025
+          Répartition de la part des dépenses d&apos;assurance dans le budget
+          des communes en 2025
         </p>
         <div className="flex gap-[4px] text-[12px] md:text-[16px] text-rf-gray italic">
           Depuis 2020 <Info size={14} />
         </div>
       </div>
-      <Image
-        src={"/home-graph-insurance-premium.svg"}
-        alt={"Part des primes d’assurance - Graph"}
-        width={311}
-        height={250}
-        className="w-full lg:hidden max-w-[500px] self-center"
-      />
-      <Image
-        src={"/home-graph-insurance-premium-desktop.svg"}
-        alt={"Part des primes d’assurance - Graph"}
-        width={1136}
-        height={380}
-        className="hidden lg:block self-center"
-      />
+
+      <ChartContent />
+
       <Caption />
     </div>
   );
