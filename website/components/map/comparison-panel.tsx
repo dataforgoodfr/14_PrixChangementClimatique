34cd5725abded8166a8faff41e0a5dcd5543cec0
Jeremy Greze <jereze@users.noreply.github.com>
2026-05-07 10:38:23 +0200

    change vulnerability index mode from discrete to continuous

--- website/components/map/comparison-panel.tsx
+++ website/components/map/comparison-panel.tsx
@@ -121,7 +121,7 @@ export function ComparisonPanel({
             <SectionSubtitle title="Indice de vulnérabilité" />
             {selectedCommune.indice_vulnerabilite_niveau ? (
               <RfVulnerabilityIndex
-                mode="discrete"
+                mode="continuous"
                 value={selectedCommune.indice_vulnerabilite_niveau}
               />
             ) : (
@@ -129,7 +129,7 @@ export function ComparisonPanel({
             )}
             {comparedCommune.indice_vulnerabilite_niveau ? (
               <RfVulnerabilityIndex
-                mode="discrete"
+                mode="continuous"
                 value={comparedCommune.indice_vulnerabilite_niveau}
               />
             ) : (
