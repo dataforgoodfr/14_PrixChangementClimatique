95d651a17a629e60772eec3b0b7591cec34bef67
Laurent34000 <lafrigole@hotmail.fr>
2026-06-01 11:56:43 +0200

    Récupère resultats_nationaux depuis table_resultats_nationaux

---
+++ data/dbt_pipeline/models/gold/resultats_nationaux.sql
@@ -0,0 +1,126 @@
+-- CODE POUR COLONNES IMPOTS 2024 ET 2020
+WITH impots AS (
+    SELECT
+        SUM(CASE
+            WHEN annee = 2024 AND agregat LIKE 'Impôts locaux'
+                THEN montant
+            ELSE 0
+        END) AS impots_locaux_2024,
+
+        SUM(CASE
+            WHEN annee = 2020 AND agregat LIKE 'Impôts locaux'
+                THEN montant
+            ELSE 0
+        END) AS impots_locaux_2020
+
+    FROM {{ ref('donnees_financieres_ofgl') }}
+),
+
+-- CODE ASSURANCES 2024 et 2020
+primes AS (
+    SELECT
+        SUM(CASE
+            WHEN annee = 2024
+                THEN prime_assurance
+            ELSE 0
+        END) AS primes_assurances_2024,
+
+        SUM(CASE
+            WHEN annee = 2020
+                THEN prime_assurance
+            ELSE 0
+        END) AS primes_assurances_2020
+
+    FROM {{ ref('primes_par_communes') }}
+),
+
+-- EVOLUTION PRIME ASSURANCE
+
+evol_prime AS (
+    SELECT
+        (primes_assurances_2024 - primes_assurances_2020)
+        / NULLIF(primes_assurances_2020, 0)
+        * 100 AS evolution_prime_assurance
+
+    FROM primes
+),
+
+-- PART PRIME BUDGET 2024
+
+prime_budget AS (
+    SELECT
+        SUM(CASE
+            WHEN annee = 2024
+                THEN depenses
+            ELSE 0
+        END) AS depenses_2024
+
+    FROM {{ ref('indicateurs_budget') }}
+),
+
+-- DEPENSES PAR POPULATION ANNEE 2023
+
+dep_pop AS (
+    SELECT
+        SUM(CASE
+            WHEN annee = 2023
+                THEN
+                    CASE
+                        WHEN NOT ISFINITE(depenses) THEN NULL
+                        ELSE depenses
+                    END
+            ELSE 0
+        END) AS depenses_2023,
+
+        SUM(CASE
+            WHEN annee = 2023
+                THEN population
+            ELSE 0
+        END) AS population_2023
+
+    FROM {{ ref('indicateurs_budget') }}
+),
+
+-- RATIO DETTES DEPENSE
+
+ratio_dette_dep AS (
+    SELECT
+        SUM(CASE
+            WHEN annee = 2024
+                THEN dettes
+            ELSE 0
+        END) AS dettes_2024
+
+    FROM {{ ref('indicateurs_budget') }}
+)
+
+-- CODE COLONNES RANDOM
+
+SELECT
+
+    CAST(RANDOM() AS DECIMAL(3, 2)) AS score_economique_moy,
+    CAST(RANDOM() AS DECIMAL(3, 2)) AS score_georisque_moy,
+    CAST(RANDOM() AS DECIMAL(3, 2)) AS score_assurance_moy,
+    CAST(RANDOM() AS DECIMAL(3, 2)) AS indice_vulnerabilite_moy,
+    CAST(RANDOM() AS INT) AS indice_vulnerabilite_niveau,
+    CAST(RANDOM() AS DECIMAL(3, 2)) AS part_communes_vulnerables,
+
+    CAST(p.primes_assurances_2024 AS DECIMAL(15, 2)) AS primes_assurances_2024,
+    CAST(p.primes_assurances_2020 AS DECIMAL(15, 2)) AS primes_assurances_2020,
+    CAST(i.impots_locaux_2024 AS DECIMAL(15, 2)) AS impots_locaux_2024,
+    CAST(i.impots_locaux_2020 AS DECIMAL(15, 2)) AS impots_locaux_2020,
+    CAST(e.evolution_prime_assurance AS DECIMAL(10, 2)) AS evolution_prime_assurance,
+    CAST(
+        p.primes_assurances_2024
+        / NULLIF(b.depenses_2024, 0) * 100
+        AS DECIMAL(10, 2)
+    ) AS part_prime_budget,
+    CAST(d.depenses_2023 / NULLIF(d.population_2023, 0) AS DECIMAL(10, 2)) AS depenses_per_pop,
+    CAST(r.dettes_2024 / NULLIF(b.depenses_2024, 0) AS DECIMAL(10, 5)) AS ratio_dettes_depenses
+
+FROM primes AS p
+CROSS JOIN impots AS i
+CROSS JOIN evol_prime AS e
+CROSS JOIN prime_budget AS b
+CROSS JOIN dep_pop AS d
+CROSS JOIN ratio_dette_dep AS r
