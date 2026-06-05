6743b0a9c2fafeebd2d78cca6c859b6c380270d1
Laurent34000 <lafrigole@hotmail.fr>
2026-06-05 17:48:55 +0200

    fix annee impots locaux

--- data/dbt_pipeline/models/gold/kpi_impots.sql
+++ data/dbt_pipeline/models/gold/kpi_impots.sql
@@ -24,7 +24,7 @@ SELECT
     / NULLIF(
         SUM(CASE WHEN annee = 2024 AND agregat = 'Recettes de fonctionnement' THEN montant ELSE 0 END), 0
     ) AS part_impots_locaux,
-    SUM(CASE WHEN annee = 2024 AND agregat = 'Impôts locaux' THEN montant ELSE 0 END) AS impots_locaux_2020
+    SUM(CASE WHEN annee = 2020 AND agregat = 'Impôts locaux' THEN montant ELSE 0 END) AS impots_locaux_2020
 
 FROM {{ ref('donnees_financieres_ofgl') }}
 GROUP BY code_geo
